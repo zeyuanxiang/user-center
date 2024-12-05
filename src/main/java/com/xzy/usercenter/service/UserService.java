6ac76c62c8726df0b20047203cef4ffbb730f6f1
yysxzy <1841025828@qq.com>
2024-12-05 22:55:06 +0800

    用户表设计，用户基本增删改查实现

---
+++ src/main/java/com/xzy/usercenter/service/UserService.java
@@ -0,0 +1,30 @@
+package com.xzy.usercenter.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xzy.usercenter.model.User;
+
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * 用户服务
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+public interface UserService extends IService<User> {
+
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+
+}
